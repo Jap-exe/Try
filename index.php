0159be7d352db9528d77ee1be09c3169dd9715c9
Jap-exe <82273672+Jap-exe@users.noreply.github.com>
2022-03-14 08:42:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Happy Birthday Babbi</title>
+    <title>A Simple Gift</title>
     <link rel="stylesheet" href="./Birthday/style.css">
     <script src="https://kit.fontawesome.com/b0f29e9bfe.js" crossorigin="anonymous"></script>
     <script defer src="./Birthday/main.js"></script>
